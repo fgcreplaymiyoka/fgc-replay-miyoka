4c69ef9c8148acec15615563604823a29ec61902
FGC Replay Miyoka <fgcreplaymiyoka@gmail.com>
2024-10-26 18:04:55 +0900

    Improve replay viewer and add time range option

--- miyoka/sf6/replay-viewer.py
+++ miyoka/sf6/replay-viewer.py
@@ -8,14 +8,16 @@ from miyoka.libs.bigquery import ReplayDataset
 from miyoka.libs.replay_viewer_helper import ReplayViewerHelper
 from miyoka.container import Container
 import altair as alt
+import re
+import numpy
 
 cache_ttl = 3600  # 1 hour
 
 
 @st.cache_resource(ttl=cache_ttl, show_spinner="Loading replay dataset...")
-def load_replay_dataset():
+def load_replay_dataset(time_range: str):
     replay_dataset: ReplayDataset = Container().replay_dataset()
-    return replay_dataset.get_all_rows(limit=1000)
+    return replay_dataset.get_all_rows(time_range=time_range)
 
 
 @st.cache_resource(ttl=cache_ttl, show_spinner="Loading replay storage...")
@@ -46,46 +48,6 @@ def reset_current_replay_index(*args, **kwargs):
     del st.session_state["current_replay_index"]
 
 
-replay_dataset: pd.DataFrame = load_replay_dataset()
-replay_storage: ReplayStorage = load_replay_storage()
-replay_viewer_helper: ReplayViewerHelper = load_replay_viewer_helper()
-character_list: list[str] = load_character_list()
-player_name = replay_viewer_helper.player_name
-
-###############################################################################################
-# View
-###############################################################################################
-# In production, users must enter the global password otherwise can't access the page.
-if not replay_viewer_helper.check_password():
-    st.stop()
-
-# -------------------------------------------------------------------
-st.subheader("Replay", divider=True)
-
-last_replay_index = len(replay_dataset) - 1
-
-if "current_replay_index" not in st.session_state:
-    st.session_state.current_replay_index = last_replay_index
-
-if "current_round_id" not in st.session_state:
-    st.session_state.current_round_id = 1
-
-current_row = replay_dataset.iloc[st.session_state.current_replay_index]
-replay_id = current_row["replay_id"]
-round_id = st.session_state.current_round_id
-video_path = replay_storage.get_authenticated_url(replay_id, round_id)
-
-html_string = f"""
-<video controls="" type="video/mp4" width=100% height="auto" src="{video_path}#t=1" playsinline autoplay muted></video>
-"""
-
-st.markdown(html_string, unsafe_allow_html=True)
-
-st.slider("Match", 0, last_replay_index, key="current_replay_index")
-
-left_col, middle_col, right_col = st.columns(3)
-
-
 def next_match():
     st.session_state.current_replay_index += 1
     st.session_state.current_replay_index %= len(replay_dataset)
@@ -106,11 +68,108 @@ def prev_round():
     st.session_state.current_round_id -= 1
 
 
+def render_current_row_value(key: str) -> str:
+    global current_row_player_side
+
+    player_side = 1 if key.startswith("p1") else 2
+    value = current_row[key]
+
+    if isinstance(value, numpy.ndarray):
+        value = ", ".join(value)
+
+    if current_row_player_side == player_side:
+        return f"**{value}**"
+
+    return value
+
+
+replay_viewer_helper: ReplayViewerHelper = load_replay_viewer_helper()
+
+player_name = replay_viewer_helper.player_name
+time_range = replay_viewer_helper.time_range
+
+replay_dataset: pd.DataFrame = load_replay_dataset(time_range)
+replay_storage: ReplayStorage = load_replay_storage()
+character_list: list[str] = load_character_list()
+
+###############################################################################################
+# View
+###############################################################################################
+# In production, users must enter the global password otherwise can't access the page.
+if not replay_viewer_helper.check_password():
+    st.stop()
+
+should_redact_pii = replay_viewer_helper.should_redact_pii()
+
+# -------------------------------------------------------------------
+st.subheader("Replay", divider=True)
+
+last_replay_index = len(replay_dataset) - 1
+
+if "current_replay_index" not in st.session_state:
+    st.session_state.current_replay_index = last_replay_index
+
+if "current_round_id" not in st.session_state:
+    st.session_state.current_round_id = 1
+
+current_row = replay_dataset.iloc[st.session_state.current_replay_index]
+current_row_player_side = (
+    1 if re.match(player_name, current_row["p1_player_name"]) else 2
+)
+replay_id = current_row["replay_id"]
+round_id = st.session_state.current_round_id
+video_path = replay_storage.get_authenticated_url(replay_id, round_id)
+
+html_string = f"""
+<video controls="" type="video/mp4" width=100% height="auto" src="{video_path}#t=1" playsinline autoplay muted></video>
+"""
+
+st.markdown(html_string, unsafe_allow_html=True)
+
+# Workaround for the column width issue
+# https://github.com/streamlit/streamlit/issues/5003#issuecomment-1276611218
+st.write(
+    """<style>
+
+[data-testid="column"] {
+    width: calc(33.3333% - 1rem) !important;
+    flex: 1 1 calc(33.3333% - 1rem) !important;
+    min-width: calc(33% - 1rem) !important;
+}
+</style>""",
+    unsafe_allow_html=True,
+)
+left_col, middle_col, right_col = st.columns(3)
+
+
 left_col.button("Next match", on_click=next_match)
 left_col.button("Prev match", on_click=prev_match)
 middle_col.button("Next round", on_click=next_round)
 middle_col.button("Prev round", on_click=prev_round)
-right_col.dataframe(current_row)
+
+
+right_col.markdown(
+    f"""
+|info|player 1|player 2|
+|---|---|---|
+|name|{render_current_row_value('p1_player_name')}|{render_current_row_value('p2_player_name')}
+|character|{render_current_row_value('p1_character')}|{render_current_row_value('p2_character')}
+|mode|{render_current_row_value('p1_mode')}|{render_current_row_value('p2_mode')}
+|result|{render_current_row_value('p1_result')}|{render_current_row_value('p2_result')}
+|round result|{render_current_row_value('p1_round_results')}|{render_current_row_value('p2_round_results')}
+|lp|{render_current_row_value('p1_lp')}|{render_current_row_value('p2_lp')}
+|mr|{render_current_row_value('p1_mr')}|{render_current_row_value('p2_mr')}
+|rank|{render_current_row_value('p1_rank')}|{render_current_row_value('p2_rank')}
+
+|metadata|value|
+|---|---|
+|replay_id|{current_row['replay_id']}|
+|played_at|{current_row['played_at']}|
+|recorded_at|{current_row['recorded_at']}|
+"""
+)
+
+st.slider("Match", 0, last_replay_index, key="current_replay_index")
 
 # -------------------------------------------------------------------
 
