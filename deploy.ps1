6543f90b2e776108a6e0f762ca4886eab5ece335
FGC Replay Miyoka <fgcreplaymiyoka@gmail.com>
2025-01-07 08:55:35 +0900

    respect gcp project id in deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -7,6 +7,7 @@ get-content .\.env | foreach {
     set-content env:\$name $value
 }
 
+$google_cloud_platform_project_id = $Env:MIYOKA_GCP_PROJECT_ID
 $google_cloud_platform_region = $Env:MIYOKA_GCP_REGION
 $gcp_replay_viewer_service_account = $Env:MIYOKA_GCP_SERVICE_ACCOUNTS_REPLAY_VIEWER_EMAIL
 $secret_id = $Env:MIYOKA_GCP_SECRETS_CONFIG_ID
@@ -17,10 +18,12 @@ $config_path = Join-Path $PSScriptRoot "config.yaml"
 gcloud --quiet secrets delete $secret_id
 
 gcloud secrets create $secret_id `
+    --project ${google_cloud_platform_project_id} `
     --replication-policy="automatic" `
     --data-file=$config_path
 
 gcloud secrets add-iam-policy-binding $secret_id `
+    --project ${google_cloud_platform_project_id} `
     --member="serviceAccount:${gcp_replay_viewer_service_account}" `
     --role="roles/secretmanager.secretAccessor"
 
@@ -28,6 +31,7 @@ gcloud secrets add-iam-policy-binding $secret_id `
 # Otherwise, the page is reloaded every 5 minutes by default, which restarts a replay randomly.
 # See https://cloud.google.com/run/docs/triggering/websockets for more information
 gcloud run deploy ${service_name} `
+    --project ${google_cloud_platform_project_id} `
     --region ${google_cloud_platform_region} `
     --service-account=${gcp_replay_viewer_service_account} `
     --allow-unauthenticated `
